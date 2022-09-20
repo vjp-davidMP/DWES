208aadd09c90aba53c7e035ee586df719760ca8c
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-09-21 00:04:36 +0200

    Ejercicio1 completado con js

--- UT2-ConceptosBásicos/Ejercicio1/JS/Ejercicio1.js
+++ UT2-ConceptosBásicos/Ejercicio1/JS/Ejercicio1.js
@@ -1,2 +1,5 @@
 alert("Bienvenido, ¿Quiere identificarse?");
 var nombre=prompt("Introduce tu nombre: ");
+console.log("Ha entrado en el sistema "+nombre);
+document.getElementById('output').innerHTML =
+   " Bienvenido, "+ nombre;
