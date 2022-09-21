c54792abadfcb26090bb5f461269bedf05531ecc
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-09-21 17:30:43 +0200

    Ejercicio1 completado con js y con otra opc

--- UT2-ConceptosBásicos/Ejercicio1/JS/Ejercicio1.js
+++ UT2-ConceptosBásicos/Ejercicio1/JS/Ejercicio1.js
@@ -1,5 +1,16 @@
+
+//<-- Ejemplo hecho por mi -->
+
 alert("Bienvenido, ¿Quiere identificarse?");
 var nombre=prompt("Introduce tu nombre: ");
 console.log("Ha entrado en el sistema "+nombre);
 document.getElementById('output').innerHTML =
    " Bienvenido, "+ nombre;
+
+
+//Ejemplo 2 Fréderic Sánchez.
+
+//alert("Bienvenido, ¿Quiere identificarse?");
+//var nombre=prompt("Introduce tu nombre: ");
+//console.log("Ha entrado en el sistema "+nombre);
+//document.write(se introduce todo el div dentro de los parentesis).
