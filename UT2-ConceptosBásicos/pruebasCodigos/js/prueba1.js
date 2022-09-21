ee332b4e53d27bc4df4f58d3c9cc3ef6dcceb67a
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-09-21 17:55:23 +0200

    Creamos directorio prueba y la prueba1

---
+++ UT2-ConceptosBásicos/pruebasCodigos/js/prueba1.js
@@ -0,0 +1,14 @@
+
+//creamos una variable con let que solo permite a la variable datos con del tipo que se ha creado la variable
+let nombre="David";
+console.log(nombre);
+console.log(typeof nombre);
+
+//aquí creamos la variable altura con var y al principio es boolean y luego float y nos lo permite.
+var altura= true;
+console.log(altura);
+console.log(typeof altura);
+
+altura= 1,76;
+console.log(altura);
+console.log(typeof altura);
