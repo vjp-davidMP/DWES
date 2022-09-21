07cec0ba437af952fb31ff24851c82d4773726d2
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-09-21 18:55:06 +0200

    Creamos directorio prueba4 y la prueba5

---
+++ UT2-ConceptosBásicos/pruebasCodigos/prueba4/js/prueba4.js
@@ -0,0 +1,6 @@
+function sumar(precio,unidades) {
+    let resultado=precio*unidades;
+    console.log("El precio es: "+resultado);
+}
+
+sumar(4,2);
---
+++ UT2-ConceptosBásicos/pruebasCodigos/prueba5/js/prueba5.js
@@ -0,0 +1,6 @@
+function funcionSumarUno(n1) {
+    let resultado= n1+1;
+    var n1=resultado;
+    return n1;
+}
+console.log(funcionSumarUno(5));
