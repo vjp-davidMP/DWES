b579e62a4e1469e5517bb8e318fa0fb346467c66
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-09-21 18:41:05 +0200

    Creamos directorio prueba3 y la prueba3

---
+++ UT2-ConceptosBásicos/pruebasCodigos/prueba3/js/prueba3.js
@@ -0,0 +1,12 @@
+/*function saludoBienvenida(nombre) {
+    console.log("Bienvenida, "+ nombre);
+}*/
+// si no le pasamos ningun nombre nos devuelve undefined ponee bienvenida Undefined.
+//saludoBienvenida();
+
+// si le pasamos tres nombre y en la funcion solo le ponemos que le pasamos un nombre nos devuelve solo el primer nombre
+// pero si le ponemos los tres te devuelve el ultimo.
+function saludoBienvenida(nombre, nombre, nombre) {
+    console.log("Bienvenida, "+ nombre);
+}
+saludoBienvenida("david","jose"," pepe");
