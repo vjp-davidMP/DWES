ce79edb31626dc94a7e0f67c9da7f73b01b5df25
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-09-25 18:34:25 +0200

    Dia:25/09/2022- Prueba7 y Prueba 10 terminadas

--- UT2-ConceptosBásicos/pruebasCodigos/prueba10/js/prueba10.js
+++ UT2-ConceptosBásicos/pruebasCodigos/prueba10/js/prueba10.js
@@ -1,13 +1,34 @@
 // Prueba de un buche while.
 
-console.log("PreIncremento");
+console.log("\nPostIncremento");
 let i=1;
 while (i<5){
     console.log(i++);//postIncremento.
 }
 
-console.log(/nPostIncremento.);
+console.log("\nPreIncremento");
 let j=1;
-while (j>5){
-    console.log(++j);
+while (j<5){
+    console.log(++j);//preincremento
+}
+// Prueba de un bucle do...while.
+console.log("\nCon do...while");
+let value=1;
+do{
+    console.log(value++);
+}while (value<=5);
+
+
+// Prueba de un bucle for.
+console.log("\nCon un bucle for.");
+for(let indice=0;indice<5;indice++){
+    console.log(indice);
+}
+
+// Prueba inicializando varios for.
+
+console.log("\nCon varios for inicializados");
+
+for (let ind1=5,ind2=0;ind1>5 && ind2<5;ind1--,ind2++){
+console.log(ind1+" --- "+ind2);
 }
