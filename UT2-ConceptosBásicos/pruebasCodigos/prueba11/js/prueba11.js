b5b48d7117a272dd9c3a49b232f586011e671da7
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-09-26 19:53:01 +0200

    Dia:26/09/2022- Prueba 11 terminada

---
+++ UT2-ConceptosBásicos/pruebasCodigos/prueba11/js/prueba11.js
@@ -0,0 +1,8 @@
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE*2);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(typeof Number.POSITIVE_INFINITY);
+console.log(Number.POSITIVE_INFINITY/2);
