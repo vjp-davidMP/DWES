7c8a530e8474fd9a2a1264df6f1a9bfdbbe50790
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-10-05 01:04:57 +0200

    Dia:04/10/2022- Terminar Ejercicio8

--- UT2-ConceptosBásicos/EjercicioEntrega/Ejercicio8.js
+++ UT2-ConceptosBásicos/EjercicioEntrega/Ejercicio8.js
@@ -1,7 +1,7 @@
 function comprobarRequisitos(array){
     for (let i =0;i<array.length;i++){
         if (typeof array[i]!="number"){
-            alert("Error");
+            console.log(alert("Error"));
         }else{
             let rasultado=array[i]*2;
             array[i]=rasultado;
@@ -10,7 +10,7 @@ function comprobarRequisitos(array){
     if (array.every(num=>num%2==0)){
         console.log("Todos los elementos del array son pares");
     }else{
-        alert("Error");
+        console.log(alert("Error"));
     }
 }
 
---
+++ UT2-ConceptosBásicos/EjercicioEntrega/Ejercicio9.js
@@ -0,0 +1,27 @@
+function platosMostrar(mapaPlatos){
+
+    for(let[platos,array]of mapaPlatos){
+        console.log("El plato "+platos.nombre+" tiene una duracion de "+platos.duracion+" minutos con dificultad: "+platos.dificultad)
+        for (let i=0;i<array.length;i++) {
+            console.log("-" + array[i] );
+        }
+    }
+
+}
+class platos {
+
+    constructor(nombre,duracion,dificultad) {
+        this.nombre=nombre;
+        this.duracion=duracion;
+        this.dificultad=dificultad;
+    }
+}
+var mapaPlatos=new Map();
+let array=["Sal","Patatas","Chorizo","zanahoria"];
+mapaPlatos.set(new platos("Cocido", 60, 6),array);
+let array1=["Sal","Patatas","Aceite","Cebollas"];
+mapaPlatos.set(new platos("Tortilla Española", 45, 7),array1);
+let array2=["Carne","Aceite","Sal","Queso","Pimientos","Tomates","Bechamel"];
+mapaPlatos.set(new platos("Berenjenas Rellenas", 158, 7),array2);
+
+platosMostrar(mapaPlatos);
