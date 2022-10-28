9df9d601581e5c5a8ff16ad415d076593dc1eae1
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-10-28 19:22:06 +0200

    Dia:28/10/2022-> Ejercicio3 terminado

--- UT4 Programación orientada a Objetos. Ajax. Promises/Ejercicios/Ejercicio3/js/main.js
+++ UT4 Programación orientada a Objetos. Ajax. Promises/Ejercicios/Ejercicio3/js/main.js
@@ -20,14 +20,26 @@ class CicloMotor {
 
     acelerar() {
 
-        if (this.encendida === true && this.velocidadActual + this.aceleracion <= this.velocidadMaxima) {
-            this.velocidadActual += this.aceleracion;
+        if (this.encendida===true){
+            this.velocidadActual+=this.aceleracion;
+            if (this.velocidadActual>this.velocidadMaxima){
+                this.velocidadActual=this.velocidadMaxima;
+            }
         }
     }
+
 
     frenar() {
         if(this.encendida===true && this.velocidadActual-this.desaceleracion>=0){
             this.velocidadActual-=this.desaceleracion;
+        }
+    }
+
+    conversionEncendido(){
+        if (this.encendida===false){
+            return "No";
+        }else{
+            return "Sí";
         }
     }
 
@@ -38,7 +50,7 @@ class CicloMotor {
         console.log(" Velocidad Actual: "+this.velocidadActual);
         console.log(" Aceleración: "+this.aceleracion);
         console.log(" Desaceleracion: "+this.desaceleracion);
-        console.log(" Encendida: "+this.encendida);
+        console.log(" Encendida: "+this.conversionEncendido());
     }
 
 
@@ -50,3 +62,113 @@ ciclomotor.mostrarInfo();
 console.log("LA MOTO TRAS ACELERAR:");
 ciclomotor.acelerar();
 ciclomotor.mostrarInfo();
+
+console.log("LA MOTO TRAS ARRANCARLA Y ACELERAR");
+ciclomotor.arrancar();
+ciclomotor.acelerar();
+ciclomotor.mostrarInfo();
+
+console.log("VOLVEMOS A ACELERAR:");
+ciclomotor.acelerar();
+ciclomotor.mostrarInfo();
+
+console.log("FRENAMOS LA MOTO:");
+ciclomotor.frenar();
+ciclomotor.mostrarInfo();
+
+class motoCross extends CicloMotor{
+    constructor(marca,aceleracion, desceleracion) {
+        super(marca, aceleracion, desceleracion);
+        this.velocidadMaxima=90;
+        this.marchaActual=0;
+    }
+
+    arrancar() {
+        super.arrancar();
+        console.log("Se levanta la pata de cabra.")
+    }
+
+    acelerar() {
+        super.acelerar();
+        if(this.velocidadActual===0){
+            this.marchaActual=0;
+        }else {
+            if(this.velocidadActual>0 && this.velocidadActual<=10){
+                this.marchaActual=1;
+            }else{
+                if(this.velocidadActual>=10 && this.velocidadActual<30){
+                    this.marchaActual=2;
+                }else{
+                    this.marchaActual=3;
+                }
+            }
+        }
+        return this.marchaActual;
+    }
+
+    mostrarInfo() {
+        super.mostrarInfo();
+        console.log(" Marcha: "+this.marchaActual);
+    }
+
+
+}
+
+console.log("\n\nPruebas de la moto de motoCross\n\n");
+
+let motocross=new motoCross("Honda",10,5);
+console.log("LA MOTO DE MOTOCROSS INICIALMENTE: ");
+motocross.mostrarInfo();
+console.log("LA MOTO TRAS ACELERAR: ");
+motocross.acelerar();
+motocross.mostrarInfo();
+console.log("LA MOTO TRAS ARRANCARLA Y ACELERAR: ");
+motocross.arrancar();
+motocross.acelerar();
+motocross.mostrarInfo();
+
+console.log("VOLVEMOS A ACELERAR: ");
+motocross.acelerar();
+motocross.mostrarInfo();
+
+console.log("FRENAMOS LA MOTO: ");
+motocross.frenar();
+motocross.mostrarInfo();
+
+
+class Scooter extends CicloMotor{
+    constructor(marca) {
+        super(marca);
+        this.aceleracion=25;
+        this.desaceleracion=15;
+    }
+
+    arrancar() {
+        this.encendida=true;
+        console.log("Se acerca la llave y se pulsa el boton, la moto arranca.");
+    }
+
+
+}
+
+console.log("\n\nPruebas de la Scooter\n\n");
+let scooter=new Scooter("Kymco");
+console.log("LA MOTO SCOOTER INICIALMENTE: ");
+scooter.mostrarInfo();
+
+console.log("LA MOTO TRAS ACELERAR: ");
+scooter.acelerar();
+scooter.mostrarInfo();
+
+console.log("LA MOTO TRAS ARRANCARLA Y ACELERAR: ");
+scooter.arrancar();
+scooter.acelerar();
+scooter.mostrarInfo();
+
+console.log("VOLVEMOS A ACELERAR: ");
+scooter.acelerar();
+scooter.mostrarInfo();
+
+console.log("FRENAMOS LA MOTO: ");
+scooter.frenar();
+scooter.mostrarInfo();
