6b784ce21aaf34755d70746a906f6a9b7eacbecd
1ºDAW - David Moreno Pajares <dmorenop08@informatica.iesvalledeljerteplasencia.es>
2022-12-13 20:35:49 +0100

    Solo queda la mejora Proyecto

--- Proyecto/js/editar.js
+++ Proyecto/js/editar.js
@@ -1,4 +1,6 @@
-let botonResena = document.querySelector("#anadir");
+let boton=document.querySelector("#editar");
+let url = window.location.search
+console.log(url);
 let cajaNombre = document.querySelector("#nombre");
 let cajaMarca = document.querySelector("#marca");
 let visita = document.querySelector("#visita");
@@ -8,8 +10,22 @@ let cajaMecanico = document.querySelector("#mecanico");
 let cajaDescripcion = document.querySelector("#descripcion");
 let cajaFecha=document.querySelector("#fecha");
 
-
-botonResena.addEventListener("click", () => {
+let params = new URLSearchParams(url);
+let uri=params.get("id");
+fetch("http://localhost:3000/opiniones/"+uri)
+    .then(response=>response.json())
+    .then(objJSON=>{
+        console.log(objJSON);
+    cajaNombre.value=objJSON.id;
+    cajaMarca.value=objJSON.modeloCoche;
+    visita.value=objJSON.motivoVisita;
+    cajaDuracion.value=objJSON.duracionReparacion;
+    cajaDescripcion.value=objJSON.descripcionOpi;
+    cajaRecomendacion.value=objJSON.recomendariaServicios;
+    cajaMecanico.value=objJSON.mecanicoAtendido;
+    cajaFecha.value=objJSON.fechaOpinion;
+}).catch(error=>console.log(error.message));
+boton.addEventListener("click",()=>{
     let resena = {
         'id': cajaNombre.value,
         'modeloCoche': cajaMarca.value,
@@ -19,23 +35,19 @@ botonResena.addEventListener("click", () => {
         'recomendariaServicios': cajaRecomendacion.value,
         'mecanicoAtendido': cajaMecanico.value,
         'fechaOpinion': cajaFecha.value
-
-
     };
-
     let options = {
-        method: 'POST',
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(resena)
     };
-    fetch('http://localhost:3000/opiniones', options)
+    fetch('http://localhost:3000/opiniones/'+cajaNombre.value, options)
         .then(data => {
             return data.json();
         }).then(resena => {
-        console.log(resena);
-
+        console.log(resena)
+        window.location.href="index.html";
     });
-});
-
+})
--- Proyecto/js/main.js
+++ Proyecto/js/main.js
@@ -1,38 +1,82 @@
 let boton = document.querySelector("#botonRese");
+let contenedor = document.querySelector("#cajaDatos");
+
+
+
+
 
 function funcionAnadir() {
-    let contenedor = document.querySelector("#cajaDatos");
-    contenedor.innerHTML="";
+
+
     fetch("http://localhost:3000/opiniones").then(response => response.json()).then(opinionesJSON => {
         console.log(opinionesJSON);
+        let arrayResena=[];
         for (let opinionesJSONElement of opinionesJSON) {
-            console.log(opinionesJSONElement);
-            contenedor.innerHTML += `<div id="todo"><p>Fecha de la Reseña: ${opinionesJSONElement.fechaOpinion}</p>
+            arrayResena.push(new Resena(opinionesJSONElement));
+        }
+        return arrayResena;
+
+    }).then(arrayResena=>{
+        for (let arrayResenaElement of arrayResena) {
+            arrayResenaElement.toDiv();
+        }
+    })
+    .catch(error => console.log(error.message));
+
+}
+boton.addEventListener("click", funcionAnadir);
+class Resena {
+    constructor(reseina) {
+        this.id = reseina.id;
+        this.modeloCoche = reseina.modeloCoche;
+        this.motivoVisita = reseina.motivoVisita;
+        this.duracionReparacion = reseina.duracionReparacion;
+        this.descripcionOpi = reseina.descripcionOpi;
+        this.recomendariaServicios = reseina.recomendariaServicios;
+        this.mecanicoAtendido = reseina.mecanicoAtendido;
+        this.fechaOpinion=reseina.fechaOpinion;
+
+    }
+
+    toDiv() {
+        let div = document.createElement("div");
+        div.setAttribute("class", "cajaPrincipal");
+        contenedor.appendChild(div);
+
+
+        div.innerHTML += `<div id="todo">
+                <p>Fecha de la Reseña: ${this.fechaOpinion}</p>
         <div id="logoTaller">
             <i class="fa-solid fa-car"><br>Talleres Jofel</i>
         </div>
         <div id="texto">
-            <p>Id: ${opinionesJSONElement.id}</p>
-            <p>Modelo de Vehiculo: ${opinionesJSONElement.modeloCoche}</p>
-            <p>Motivo de visita: ${opinionesJSONElement.motivoVisita}</p>
-            <p>Duración Reparación: ${opinionesJSONElement.duracionReparacion}</p>
-            <p>Valoracion: ${opinionesJSONElement.descripcionOpi}</p>
-            <p>Recomendarias Servicios: ${opinionesJSONElement.recomendariaServicios}</p>
-            <p>Mecanico: ${opinionesJSONElement.mecanicoAtendido}</p>
+            <p>Id: ${this.id}</p>
+            <p>Modelo de Vehiculo: ${this.modeloCoche}</p>
+            <p>Motivo de visita: ${this.motivoVisita}</p>
+            <p>Duración Reparación: ${this.duracionReparacion}</p>
+            <p>Valoracion: ${this.descripcionOpi}</p>
+            <p>Recomendarias Servicios: ${this.recomendariaServicios}</p>
+            <p>Mecanico: ${this.mecanicoAtendido}</p>
         </div>
-        <button class="boton" id="borrar">Borrar</button>
-        <button class="boton">Editar</button></div>`;
-0
-            let bBorrar=contenedor.querySelector("#borrar");
-            let cuadro=contenedor.querySelector("#todo");
-            bBorrar.addEventListener("click",()=>{
-                let url="http://localhost:3000" +
-                    "/opiniones"+opinionesJSONElement.id
-                fetch(url, { method: 'DELETE' })
-                    .then(() => cuadro.innerHTML = '');
-            });
-        }
-    }).catch(error=>console.log(error.message));
+        <button class="borrar">Borrar</button>
+        <button class="editar">Editar</button>
+        </div>`
+
+        let botonBorrar=div.querySelector(".borrar");
+
+        botonBorrar.addEventListener("click",()=>{
+            console.log(this.id);
+            if (confirm("¿Quiere borrar esta reseña?")===true){
+                fetch("http://localhost:3000/opiniones/"+this.id, {
+                    method: 'DELETE',
+                }).catch(erro=>console.log(erro.message));
+                div.remove();
+            }
+        });
+
+        let botonEditar=div.querySelector(".editar");
+        botonEditar.addEventListener("click",()=>{
+     window.location.href="editar.html?id="+this.id;
+        });
+    }
 }
-boton.addEventListener("click",funcionAnadir);
-console.log("hola");
